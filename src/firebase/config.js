aa9720fedd0510a5a648faaabc6e3873eba710fc
Mohsinali Chaudhary <ali97730@gmail.com>
2020-10-12 00:06:58 +0530

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyAFkezbQEhcIKgbuorCC31bhSKsiGoTEys",
+  authDomain: "confusionserver-2cb6f.firebaseapp.com",
+  databaseURL: "https://confusionserver-2cb6f.firebaseio.com",
+  projectId: "confusionserver-2cb6f",
+  storageBucket: "confusionserver-2cb6f.appspot.com",
+  messagingSenderId: "90009779553"
+};
